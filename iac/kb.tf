ac6381366f79c4b767f6bc146023886d3737ede9
John Ritsema <john_ritsema@yahoo.com>
2024-10-29 16:50:21 -0400

    adds kb sync logs to kb-sync.tf (#2)

--- iac/kb.tf
+++ iac/kb.tf
@@ -154,35 +154,3 @@ resource "aws_scheduler_schedule" "main" {
     mode = "OFF"
   }
 }
-
-resource "aws_iam_role" "event_bridge_scheduler" {
-  name = "${var.name}-event-bridge-scheduler-role"
-
-  assume_role_policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "scheduler.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
-}
-
-data "aws_iam_policy_document" "kb_sync" {
-  statement {
-    effect    = "Allow"
-    actions   = ["bedrock:StartIngestionJob"]
-    resources = [aws_bedrockagent_knowledge_base.main.arn]
-  }
-}
-
-resource "aws_iam_role_policy" "event_bridge_scheduler" {
-  role   = aws_iam_role.event_bridge_scheduler.name
-  policy = data.aws_iam_policy_document.kb_sync.json
-}
