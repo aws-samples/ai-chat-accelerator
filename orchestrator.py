4cb859993790cf107b21812b03e7235f34e1a43f
John Ritsema <john_ritsema@yahoo.com>
2024-12-10 11:59:10 -0500

    optimizes the accuracy of follow up questions (#10)
    This change adds an LLM call for follow up questions that rewords the question based on context from previous questions. The reworded question is used to query the knowledge base for increased accuracy.

--- orchestrator.py
+++ orchestrator.py
@@ -8,14 +8,27 @@ def orchestrate(conversation_history, new_question):
     and a new question. Returns an answer and a list of
     source documents."""
 
+    # get model specific prompts
+    system_prompt, user_prompt, reword_prompt = claude3_haiku_v1.get_prompts()
+    query = new_question
+
+    # for follow up questions, use llm to re-word with context from
+    # previous questions in the conversation
+    if len(conversation_history["questions"]) > 0:
+        past_q = [question["q"]
+                  for question in conversation_history["questions"]]
+        p = reword_prompt.format(
+            past_questions=json.dumps(past_q, indent=2),
+            new_question=new_question,
+        )
+        messages = [{"role": "user", "content": [{"text": p}]}]
+        query = claude3_haiku_v1.generate_message(messages)
+
     # retrieve data from vector db based on question
-    docs = bedrock_kb.get_relevant_docs(new_question, 6)
+    docs = bedrock_kb.get_relevant_docs(query, 6)
 
     # normalize source documents
     sources = bedrock_kb.format_sources(docs)
-
-    # get model specific prompts
-    system_prompt, user_prompt = claude3_haiku_v1.get_prompts()
 
     # build prompt based on new question and search results
     context = json.dumps(docs)
@@ -33,6 +46,7 @@ def orchestrate(conversation_history, new_question):
     messages.append({"role": "user", "content": [{"text": prompt}]})
 
     # invoke LLM
-    response = claude3_haiku_v1.generate_message(system_prompt, messages)
+    response = claude3_haiku_v1.generate_message(
+        messages, system_prompt=system_prompt)
 
     return response, sources
