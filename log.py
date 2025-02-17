f5076b170dbebccc5cfed0ed375206543f418131
John Ritsema <john_ritsema@yahoo.com>
2025-02-17 14:17:17 -0500

    fixes issue with logging level

--- log.py
+++ log.py
@@ -12,13 +12,13 @@ log.setLevel(logging.ERROR)
 
 def debug(obj):
     """log object as json if in debug mode"""
-    if logging.getLogger().level == logging.DEBUG:
+    if logging.getLogger().level <= logging.DEBUG:
         logging.debug(json.dumps(obj, indent=2, default=str))
 
 
 def info(obj):
-    """log object as json if in info mode"""
-    if logging.getLogger().level == logging.INFO:
+    """log object as json if in info or debug mode"""
+    if logging.getLogger().level <= logging.INFO:
         logging.info(json.dumps(obj, indent=2, default=str))
 
 
